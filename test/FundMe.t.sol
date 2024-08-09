f5ca9da06b1364890431749e1fd3cccdd6c8cc05
Sardius <0xsardius@gmail.com>
2024-08-09 16:48:52 -0400

    added cheaperwithdraw

--- test/FundMe.t.sol
+++ test/FundMe.t.sol
@@ -11,6 +11,7 @@ contract FundMeTest is Test {
     address USER = makeAddr("user");
     uint256 constant SEND_VALUE = 0.1 ether;
     uint256 constant STARTING_BALANCE = 10 ether;
+    uint256 constant GAS_PRICE = 1;
 
     function setUp() external {
         //    fundMe = new FundMe(0x4aDC67696bA383F43DD60A9e78F2C97Fbbfc7cb1);
@@ -72,8 +73,16 @@ contract FundMeTest is Test {
         uint256 startingOwnerBalance = fundMe.getOwner().balance;
         uint256 startingFundMeBalance = address(fundMe).balance;
         // Act
+        // tells you how much gas left in your tx call
+        uint256 gasStart = gasleft();
+        vm.txGasPrice(GAS_PRICE);
         vm.prank(fundMe.getOwner());
         fundMe.withdraw();
+
+        uint256 gasEnd = gasleft();
+        uint256 gasUsed = (gasStart - gasEnd) * tx.gasprice;
+        console.log(gasUsed);
+
         // Assert
         uint256 endingOwnerBalance = fundMe.getOwner().balance;
         uint256 endingFundMeBalance = address(fundMe).balance;
@@ -81,7 +90,7 @@ contract FundMeTest is Test {
         assertEq(startingFundMeBalance + startingOwnerBalance, endingFundMeBalance);
     }
     
-    function testWithdrawFromMultipleFunders() public funded {
+    function testWithdrawFromMultipleFundersCheaper() public funded {
         // Arrange
         uint160 numberOfFunders = 10;
         // Don't send to 0 address, it can glitch
@@ -92,12 +101,16 @@ contract FundMeTest is Test {
         }
         uint256 startingOwnerBalance = fundMe.getOwner().balance;
         uint256 startingFundMeBalance = address(fundMe).balance;
+
+
         vm.prank(fundMe.getOwner());
-        fundMe.withdraw();
+        fundMe.cheaperWithdraw();
         vm.stopPrank();
 
         // Assert
         assert(address(fundMe).balance == 0);
         assert(startingFundMeBalance + startingOwnerBalance == fundMe.getOwner().balance);
     }
+    
+    
 }
